5071b5be68b99c6008ac84055dedcfa2d8f65518
Brooke Lewis <belv2c@gmail.com>
2018-01-22 15:37:29 -0600

    finished assignment

--- TheFamilyDictionary/Program.cs
+++ TheFamilyDictionary/Program.cs
@@ -11,13 +11,41 @@ namespace TheFamilyDictionary
         static void Main(string[] args)
         {
 
-        Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
 
-        myFamily.Add("sister", new Dictionary<string, string>(){
-            {"name", "Mom"},
+            myFamily.Add("mom", new Dictionary<string, string>(){
+            {"name", "Amy"},
             {"age", "45"}
-
         });
+
+            myFamily.Add("dad", new Dictionary<string, string>(){
+            {"name", "Scott"},
+            {"age", "50"}
+        });
+
+            myFamily.Add("brother", new Dictionary<string, string>(){
+            {"name", "Alex"},
+            {"age", "20"},
+            {"name", "Zach"},
+            {"age", "16"}
+        });
+
+
+            foreach (var relative in myFamily)
+            {
+                foreach (var person in relative.Value)
+                {
+                    if (person.Key == "name")
+                    {
+                        Console.Write($"{person.Value} is my {relative.Key}");
+                    }
+                    else if (person.Key == "age")
+                    {
+                        Console.Write($"and is {person.Value} years old!!!!!");
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
