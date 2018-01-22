85973a3b9666833d83fb98a83ad4fa9d009060c6
Brooke Lewis <belv2c@gmail.com>
2018-01-22 15:23:11 -0600

    setup project

--- TheFamilyDictionary/Program.cs
+++ TheFamilyDictionary/Program.cs
@@ -10,6 +10,14 @@ namespace TheFamilyDictionary
     {
         static void Main(string[] args)
         {
+
+        Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+        myFamily.Add("sister", new Dictionary<string, string>(){
+            {"name", "Mom"},
+            {"age", "45"}
+
+        });
         }
     }
 }
